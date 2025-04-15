1c1738fdb7a5d0dca7937f3fe6f82aa3ed99d7f9
ShahedAnnam <shahedanam2018@gmail.com>
2025-04-15 21:50:15 +0600

    dd

--- EduLearn/settings.py
+++ EduLearn/settings.py
@@ -36,6 +36,8 @@ SECRET_KEY =env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
+
 if ENVIRONMENT == "development":
     DEBUG = True
     ALLOWED_HOSTS = []
