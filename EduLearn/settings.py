668c5a16964f6255fba9375c8ffcfac919cee488
ShahedAnnam <shahedanam2018@gmail.com>
2025-04-15 21:54:31 +0600

    development

--- EduLearn/settings.py
+++ EduLearn/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY =env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
+ENVIRONMENT = os.getenv("ENVIRONMENT", default="development")
 
 if ENVIRONMENT == "development":
     DEBUG = True
